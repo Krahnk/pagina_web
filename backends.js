738c60c4a7d1d034fe6946014f6c052cc4b42449
Nahuel <pascu.hammerly@gmail.com>
2025-12-22 16:39:56 -0300

    .

--- backends.js
+++ backends.js
@@ -1,7 +1,6 @@
 const cursor = document.getElementById("cursor");
 
 function evento1() {
-  alert("lo apretaste!!");
   document.getElementById("mensaje").innerHTML = "Eso fue genial.. No?";
   cursor.style.display = "block"; 
 }
