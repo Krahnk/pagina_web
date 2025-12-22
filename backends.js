df6a2dbedb1e3209604e234cf07f977494936ac3
Krahnk <pascu.hammerly@gmail.com>
2025-12-22 20:57:32 -0300

    Update backends.js

--- backends.js
+++ backends.js
@@ -1,16 +1,20 @@
-const cursor = document.getElementById("cursor");
-
+let cursor;
 let X = 0;
 let Y = 0;
 
+document.addEventListener("DOMContentLoaded", () => {
+  cursor = document.getElementById("cursor");
+
+  document.addEventListener("mousemove", (e) => {
+    X += (e.clientX - X) * 0.05;
+    Y += (e.clientY - Y) * 0.05;
+
+    cursor.style.left = X + "px";
+    cursor.style.top  = Y + "px";
+  });
+});
+
 function evento1() {
   document.getElementById("mensaje").innerHTML = "Eso fue genial.. No?";
-  cursor.style.display = "block"; 
+  cursor.style.display = "block";
 }
-
-document.addEventListener("mousemove", (e) => {
-    X += (e.clientX - X ) * 0.5;
-    Y += (e.clientY - Y ) * 0.5;
-  cursor.style.left = X + "px";
-  cursor.style.top = Y + "px";
-});
