8c08bc16ec5ae85c5cdbcfaeee09035a3dd3b7d9
Krahnk <pascu.hammerly@gmail.com>
2025-12-22 21:02:39 -0300

    Update backends.js

--- backends.js
+++ backends.js
@@ -6,8 +6,8 @@ document.addEventListener("DOMContentLoaded", () => {
   cursor = document.getElementById("cursor");
 
   document.addEventListener("mousemove", (e) => {
-    X += (e.clientX - X) * 0.005;
-    Y += (e.clientY - Y) * 0.005;
+    X += (e.clientX - X) * 0.00005;
+    Y += (e.clientY - Y) * 0.00005;
 
     cursor.style.left = X + "px";
     cursor.style.top  = Y + "px";
