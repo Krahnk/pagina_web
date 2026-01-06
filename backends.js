ac2409d8c13680ec82b8b2ed124212cb8948c2af
Krahnk <pascu.hammerly@gmail.com>
2026-01-06 15:32:00 -0300

    Update backends.js

--- backends.js
+++ backends.js
@@ -1,18 +1,35 @@
 let cursor;
 let X = 0;
 let Y = 0;
+let mouseX = 0;
+let mouseY = 0;
+const speed = 0.05;
 
 document.addEventListener("DOMContentLoaded", () => {
   cursor = document.getElementById("cursor");
+});
 
-  document.addEventListener("mousemove", (e) => {
-    X += (e.clientX - X) * 0.005;
-    Y += (e.clientY - Y) * 0.005;
+//actualizar la posicion del mouse
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
 
+function animar() {
+  X += (mouseX - X) * speed;
+  Y += (mouseY - Y) * speed;
+
+  if (cursor) { 
     cursor.style.left = X + "px";
     cursor.style.top  = Y + "px";
-  });
-});
+  }
+
+  requestAnimationFrame(animar); 
+}
+
+// inicia el loop
+animar(); 
+
 
 function evento1() {
   document.getElementById("mensaje").innerHTML = "Eso fue genial.. No?";
